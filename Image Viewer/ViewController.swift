55ff121a902d6705f735b8eed5626402c28bf9ff
Łukasz Nycz <lnycz96@gmail.com>
2021-06-26 11:42:31 +0200

    Initial Commit

---
+++ Image Viewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image Viewer
+//
+//  Created by Łukasz Nycz on 26/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
