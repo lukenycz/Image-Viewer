c9df6d1cbc5961871e2e59eb30a936940e28d7d7
Łukasz Nycz <lnycz96@gmail.com>
2021-07-24 12:23:13 +0200

    save data using Codable

--- Image Viewer/ViewController.swift
+++ Image Viewer/ViewController.swift
@@ -9,35 +9,58 @@ import UIKit
 
 class ViewController: UITableViewController {
 
-    struct Model {
+    class Model: NSObject, Codable {
         let picture: String
         var count = 0
         
-        init(picture: String) {
+        init(picture: String, count: Int) {
             self.picture = picture
+            self.count = count
         }
     }
-    
     var pictures = [Model]() {
         didSet {
             tableView.reloadData()
         }
     }
-    var viewCounter = 0
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Image Viewer"
         navigationController?.navigationBar.prefersLargeTitles = true
         
-       performSelector(inBackground: #selector(fetchData), with: nil)
+        performSelector(inBackground: #selector(fetchData), with: nil)
         
         tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
-                
+        
         pictures = pictures.sorted(by: { $0.picture < $1.picture })
+        
+        tableView.reloadData()
     }
+    override func viewDidAppear(_ animated: Bool) {
+        let defaults = UserDefaults.standard
+         if let savedCounter = defaults.object(forKey: "pictures") as? Data {
+             let jsonDecoder = JSONDecoder()
+
+             do {
+                 pictures = try jsonDecoder.decode([Model].self, from: savedCounter)
+             } catch {
+                 print("Failed to load counter")
+             }
+         }
+    }
+    
+    func save() {
+        let jsonEncoder = JSONEncoder()
+        if let savedData = try? jsonEncoder.encode(pictures) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "pictures")
+        } else {
+            print("Failed to save counter.")
+        }
+    }
+    
+    
     @objc func fetchData() {
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
@@ -46,7 +69,7 @@ class ViewController: UITableViewController {
         DispatchQueue.main.async { [weak self] in
             for item in items {
                 if item.hasPrefix("nssl") {
-                    self?.pictures.append(Model(picture: item))
+                    self?.pictures.append(Model(picture: item, count: 0))
                 }
             }
         }
@@ -64,12 +87,14 @@ class ViewController: UITableViewController {
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
-            var model = pictures[indexPath.row]
+            let model = pictures[indexPath.row]
             navigationController?.pushViewController(vc, animated: true)
             vc.detailVCTitle = "Picture \(indexPath.row + 1) of \(pictures.count)"
             vc.selectedImage = model.picture
             model.count += 1
+            save()
             pictures[indexPath.row] = model
+
         }
     }
 }
